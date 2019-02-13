0f4717edf4a3271d84da338f5ff86014e2c55a0d
andradamaja <andradamaja@gmail.com>
2019-02-13 13:48:23 +0200

    Tema1 initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Andrada";
+        System.out.println("Hello\r\n" + name);
+
+        System.out.println();
+
+        byte a1 = 7;
+        byte a2 = 8;
+        System.out.println("The addition of 7 and 8 is: " + (a1 + a2));
+
+        System.out.println();
+
+        float d1 = 3;
+        float d2 = 2;
+        float division = d1 / d2;
+        System.out.println(division);
+
+        System.out.println();
+
+        short a = -5 + 8 * 6;
+        byte b = (55 + 9) % 9;
+        d1 = 20 + -3f * 5 / 8;
+        a1 = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println("a. " + a);
+        System.out.println("b. " + b);
+        System.out.println("c. " + d1);
+        System.out.println("d. " + a1);
+
+
+
+    }
+}
