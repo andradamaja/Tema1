89fd8a46117cfeb22eb54ddf1c47e283111e6fc1
andradamaja <andradamaja@gmail.com>
2019-02-19 11:01:36 +0200

    Tema1 metode

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Andrada";
-        System.out.println("Hello\r\n" + name);
 
-        System.out.println();
+        printHelloName();
 
-        byte a1 = 7;
-        byte a2 = 8;
-        System.out.println("The addition of 7 and 8 is: " + (a1 + a2));
+        printSum();
 
-        System.out.println();
+        Scanner scan = new Scanner(System.in);
 
-        float d1 = 3;
-        float d2 = 2;
-        float division = d1 / d2;
-        System.out.println(division);
+        System.out.print("Enter two numbers: ");
 
-        System.out.println();
+        float d1 = scan.nextFloat();
 
-        short a = -5 + 8 * 6;
-        byte b = (55 + 9) % 9;
-        d1 = 20 + -3f * 5 / 8;
-        a1 = 5 + 15 / 3 * 2 - 8 % 3;
+        float d2 = scan.nextFloat();
 
-        System.out.println("a. " + a);
-        System.out.println("b. " + b);
-        System.out.println("c. " + d1);
-        System.out.println("d. " + a1);
+        float div = division(d1, d2);
 
+        System.out.println(d1+ " / " + d2 + " = " + div);
 
+        printOps();
 
     }
+
+    //1. Write a Java program to print 'Hello' on the screen and then print your name on a separate line.
+
+    public static void printHelloName() {
+        System.out.println("Hello\r\nAndrada");
+    }
+
+    //2.Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers.
+
+    public static void printSum() {
+        System.out.println("The addition of 7 and 8 is: " + (7 + 8));
+    }
+
+    //3.Write a Java program to divide two numbers and print on the screen.
+
+    public static float division(float x, float y){
+        return x / y;
+    }
+
+//    4.Write a Java program to print the result of the following operations:
+//    a. -5 + 8 * 6
+//    b. (55+9) % 9
+//    c. 20 + -3*5 / 8
+//    d. 5 + 15 / 3 * 2 - 8 % 3
+
+    public static void printOps(){
+        short a = -5 + 8 * 6;
+        byte b = (55 + 9) % 9;
+        float c = 20 + -3f * 5 / 8;
+        short d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("a. " + a + ";\n" + "b. " + b + ";\n" + "c. " + c + ";\n" + "d. " + d + ";");
+    }
+
 }
